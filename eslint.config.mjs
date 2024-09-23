623201e48a625faecdcd4a71b80a46d4366d6675
VadzimRudz <vadzim_rudz@senla.eu>
2024-09-23 11:27:29 +0200

    add ignores

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,29 +1,30 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import googleConfig from "eslint-config-google";
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import googleConfig from 'eslint-config-google';
 
 export default [
-{ files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
-{ languageOptions: { globals: globals.browser } },
-pluginJs.configs.recommended,
-{
-...googleConfig,
-languageOptions: {
-globals: {
-...globals.browser,
-describe: "readonly",
-it: "readonly",
-before: "readonly",
-after: "readonly",
-beforeEach: "readonly",
-afterEach: "readonly"
-}
-},
-rules: {
-...googleConfig.rules,
-"valid-jsdoc": "off",
-"require-jsdoc": "off",
-"linebreak-style": "off"
-}
-}
+  {ignores: ['node_modules', 'mochawesome-report']},
+  {files: ['**/*.js'], languageOptions: {sourceType: 'commonjs'}},
+  {languageOptions: {globals: globals.browser}},
+  pluginJs.configs.recommended,
+  {
+    ...googleConfig,
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        describe: 'readonly',
+        it: 'readonly',
+        before: 'readonly',
+        after: 'readonly',
+        beforeEach: 'readonly',
+        afterEach: 'readonly',
+      },
+    },
+    rules: {
+      ...googleConfig.rules,
+      'valid-jsdoc': 'off',
+      'require-jsdoc': 'off',
+      'linebreak-style': 'off',
+    },
+  },
 ];
